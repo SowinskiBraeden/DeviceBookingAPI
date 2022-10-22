10bc974d2dcd5efd78030b4a63bddae415b94849
SowinskiBraeden <sowinskibraeden@gmail.com>
2022-10-21 17:12:10 -0700

    fix device details name and capitalize database queries

--- api/handlers/device.go
+++ api/handlers/device.go
@@ -163,7 +163,7 @@ func (d Device) UpdateDeviceHandler(w http.ResponseWriter, r *http.Request) {
 		varName := e.Type().Field(i).Name
 		varValue := e.Field(i).Interface()
 		if varValue != nil && varValue != "" {
-			update["Device."+varName] = varValue
+			update["Details."+varName] = varValue
 		}
 	}
 
@@ -188,7 +188,7 @@ func (d Device) GetChildDevices(w http.ResponseWriter, r *http.Request) {
 	cowID := mux.Vars(r)["cow_id"]
 	defer cancel()
 
-	devices, _ := d.DB.Find(ctx, bson.M{"detials.parent": cowID})
+	devices, _ := d.DB.Find(ctx, bson.M{"Detials.Parent": cowID})
 
 	// If there is no devices from the query, return empty device array.
 	if len(devices) == 0 {
